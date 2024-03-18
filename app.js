ef373cb84e04f442bb77ff7b1d37f56d8223cba6
Facundo Munoz <facundomartinmunoz@gmail.com>
2024-03-18 19:28:29 +0800

    add tailwind css

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
   const allPosts = await posts.findAll();
